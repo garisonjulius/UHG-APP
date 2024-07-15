ca99492b336b240e6e408fb7b65ff586549ece4f
pujapatel03 <puja.patel@publicissapient.com>
2024-07-15 10:40:53 -0500

    updated run.py to have langchain implementation

--- backend/run.py
+++ backend/run.py
@@ -1,25 +1,39 @@
 from flask import Flask
-import os
-from openai import AzureOpenAI
+from langchain_openai import AzureOpenAI
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
 
 app = Flask(__name__)
 
-client = AzureOpenAI(
-    api_key=os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2024-02-01",
-    azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT")
+llm_model = AzureOpenAI(
+    deployment_name="gpt-35-turbo-instruct",
 )
+loader = TextLoader("./test.txt")
+docs = loader.load()
 
-deployment_name='gpt-35-turbo-instruct'
+embeddings =  AzureOpenAIEmbeddings(model = "text-embedding-ada-002")
+index = VectorstoreIndexCreator(embedding=embeddings).from_loaders([loader])
 
 @app.route('/')
 def hello():
     print('Sending a test completion job')
-    start_phrase = 'Who is United Health Group?'
-    response = client.completions.create(model=deployment_name, prompt=start_phrase, max_tokens=128)
-    # See how the API responds in terminal
-    print(response.choices[0])
-    return start_phrase+response.choices[0].text
+    start_phrase = "Who's is the user"
+    response = index.query(start_phrase, llm = llm_model)
+    print(response)
+
+    return start_phrase + ": " + response
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
