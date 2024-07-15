7c226cb984461662f99c5adf8a7cf1c746213741
pujapatel03 <puja.patel@publicissapient.com>
2024-07-15 14:41:53 -0500

    ai has knowledge of outside world

--- backend/run.py
+++ backend/run.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from langchain_openai import AzureOpenAI
+from langchain_openai import AzureChatOpenAI
+
 from langchain_community.document_loaders import TextLoader
+
 from langchain_openai import AzureOpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 
 app = Flask(__name__)
 
-llm_model = AzureOpenAI(
-    deployment_name="gpt-35-turbo-instruct",
+llm_model = AzureChatOpenAI(
+    azure_deployment= "gpt-35-turbo",
 )
+
 loader = TextLoader("./test.txt")
-docs = loader.load()
 
 embeddings =  AzureOpenAIEmbeddings(model = "text-embedding-ada-002")
 index = VectorstoreIndexCreator(embedding=embeddings).from_loaders([loader])
@@ -18,8 +20,8 @@ index = VectorstoreIndexCreator(embedding=embeddings).from_loaders([loader])
 @app.route('/')
 def hello():
     print('Sending a test completion job')
-    start_phrase = "Who's is the user"
-    response = index.query(start_phrase, llm = llm_model)
+    start_phrase = "who is the highest rated chess player in the world"
+    response = index.query(start_phrase, llm_model)
     print(response)
 
     return start_phrase + ": " + response
