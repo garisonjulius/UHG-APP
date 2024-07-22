88c28336101a9cd521422695e55a3f55654877ac
Ayham Huq <ayham.huq@publicissapient.com>
2024-07-22 09:59:28 -0500

    Updated health paramters.

--- backend/run.py
+++ backend/run.py
@@ -18,7 +18,7 @@ embeddings =  AzureOpenAIEmbeddings(model="text-embedding-ada-002")
 index = VectorstoreIndexCreator(embedding=embeddings).from_loaders([loader])
  
 @app.route('/')
-def hello():
+def main():
     # Context: The user's persona is Sara Hayes. She is 29, female, has a child, and has an income of $100,000. Her generic prescription spending is $50. The number of prescriptions she has is 3. The amount she has spent on emergency visits is $515. The amount she has spent on hospital benefits is $2225. She has spent $1120 on imaging. She has spent $74 on each of the specialist visits. She has spent $1500 on XRAYS. She has spent $148 on each of the doctor visits. She has had 8 labs and tests, 6 specialist visits, and 5 doctor visits. She is looking to maximize savings for her personal healthcare plan.
     # Task: Recommend a healthcare plan based on the data provided and provide reasoning for why this plan is superior. Take into account income and children when making your decision.
     # Exemplar: Please follow this example: Bob spent $15000 on generic medication last year. Since the cost of generic spending is least with the UHC Gold-X Advantage+ ($0 PCP, Adult Dental + Vision) plan, as it offers the lowest cost of $3 for generic medications before and after deductible.
