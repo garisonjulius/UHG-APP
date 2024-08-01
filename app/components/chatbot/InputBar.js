d4f7e6be7877c19e9763573b6762f30ffea534dd
Erika Hochstein <erihochs1@publicisgroupe.net>
2024-08-01 16:03:53 -0500

    Cleared the input message upon enter

--- app/components/chatbot/InputBar.js
+++ app/components/chatbot/InputBar.js
@@ -4,7 +4,6 @@ import {View, Text, TextInput, StyleSheet, TouchableOpacity, Image, Dimensions, 
 
 var windowWidth = Dimensions.get('window').width;
 import { GiftedChat } from "react-native-gifted-chat";
-import Prompts from "./Prompts"
 
 const InputBar = ({uid}) => {
 
@@ -14,11 +13,11 @@ const InputBar = ({uid}) => {
     const [promptClicked, setPromptClicked] = useState(false);
     const [userName, setUserName] = useState(null)
 
-    useEffect = () => {
+    useEffect (() => {
         fetch(`http://10.0.2.2:5000/user/${uid}`)
         .then(result => result.json())
         .then(data => setUserName(data["first_name"]))
-    }
+    }, [uid])
 
     const handlePromptProviderClick = () => {
         
@@ -31,12 +30,14 @@ const InputBar = ({uid}) => {
     const handlePromptPlanClick = () => {
         setInputMessage("My plan benefits")
         setPromptClicked(true)
+
         //handleButtonClick()
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         if(promptClicked){
             handleButtonClick();
+            setPromptClicked(false);
         }
     }, [inputMessage])
 
@@ -47,10 +48,13 @@ const InputBar = ({uid}) => {
     }
 
     const handleButtonClick = () => {
+        const tempInputMsg = inputMessage;
+        // Clear input message
+        setInputMessage("");
 
         const message = {
             _id:Math.random().toString(36).substring(7),
-            text: inputMessage,
+            text: tempInputMsg,
             createdAt: new Date(),
             user: {_id:1}
         }
@@ -58,7 +62,7 @@ const InputBar = ({uid}) => {
             GiftedChat.append(previousMessages, [message])
         )
 
-        fetch(`http://10.0.2.2:5000/getResponse/${uid}/${inputMessage}`)
+        fetch(`http://10.0.2.2:5000/getResponse/${uid}/${tempInputMsg}`)
         .then(response => response.json())
         .then(data => {
             console.log(data["chat_response"]),
@@ -115,9 +119,9 @@ const InputBar = ({uid}) => {
             <View style = {styles.inputField}>
                 <View style = {styles.inputBar}> 
                     <View style = {styles.fixWidth}>
-                        <TextInput  placeholder= "Ask me anything..." placeholderTextColor={"black"} style={styles.placeholderText} onChangeText = {handleTextInput}></TextInput>   
+                        <TextInput multiline={true} placeholder="Ask me anything..." value={inputMessage} onSubmitEditing={handleButtonClick} placeholderTextColor={"grey"} style={styles.placeholderText} onChangeText = {handleTextInput}></TextInput>   
                     </View>
-                    <TouchableOpacity onPress = {handleButtonClick}>
+                    <TouchableOpacity onPress={handleButtonClick}>
                         <View style = {styles.sendButton}>
                             <Image
                                 source={require("../../assets/arrow-right-circle.png")}
@@ -228,6 +232,6 @@ const styles = StyleSheet.create({
     },
 
     fixWidth: {
-        maxWidth: '50%',
+        maxWidth: '80%',
     },
 });
